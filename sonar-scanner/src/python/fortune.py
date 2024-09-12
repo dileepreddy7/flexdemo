a7e04ad42084620eb531251cbdad4b6f6db76680
fdadmin <dileep.pinnapureddy@flexagon.com>
2024-09-12 18:14:11 +0530

    Files committed by user fdadmin through FlexDeploy Package -Trigger_Test_Package

--- sonar-scanner/src/python/fortune.py
+++ sonar-scanner/src/python/fortune.py
@@ -2,7 +2,7 @@
 # fortune.py -- chooses a random fortune, as the fortune(8) program in
 #               the BSD-games package does
 #
-# Copyright (c) 2010, Andrew M. Kuchling
+# Copyright (c) 2010,
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
