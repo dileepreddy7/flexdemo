9cad3d654639b1a292f1f58b267647a90f7fa767
dileepreddy <55627599+dileepreddy7@users.noreply.github.com>
2024-09-18 11:51:12 +0530

    Delete sonar scan directory

--- sonar scan/travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-mvn clean install
-
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  mvn sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
-fi
